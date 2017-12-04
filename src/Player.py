eb118c7cbac17a37ee4a91f0d50df667517d2e22
Jacob Shiohira <jlshiohira23@gmail.com>
2017-12-04 00:14:07 -0600

    Change tabs to spaces and add comments to Utils.py

--- src/Player.py
+++ src/Player.py
@@ -5,32 +5,32 @@ map_player_name_to_enum = { "lower": LOWER, "UPPER": UPPER}
 
 class Player():
 
-	def __init__(self, name, initial_captures=None):
-		self.name = name
-		self.pieces = None
-		if not initial_captures:
-			self.captures = []
-		else:
-			self.captures = initial_captures
-		self.in_check = False
-		self.in_checkmate = False
-		self.escape_moves = None
-		self.moves_to_go_in_check = None
+    def __init__(self, name, initial_captures=None):
+        self.name = name
+        self.pieces = None
+        if not initial_captures:
+            self.captures = []
+        else:
+            self.captures = initial_captures
+        self.in_check = False
+        self.in_checkmate = False
+        self.escape_moves = None
+        self.moves_to_go_in_check = None
 
-	def is_in_check(self):
-		return self.in_check
+    def is_in_check(self):
+        return self.in_check
 
-	def is_in_checkmate(self):
-		return self.in_checkmate
+    def is_in_checkmate(self):
+        return self.in_checkmate
 
-	def set_pieces(self, pieces):
-		self.pieces = pieces
+    def set_pieces(self, pieces):
+        self.pieces = pieces
 
-	def get_pieces(self):
-		return self.pieces
+    def get_pieces(self):
+        return self.pieces
 
-	def update_piece_locations(self, key, val):
-		self.pieces[key] = val
+    def update_piece_locations(self, key, val):
+        self.pieces[key] = val
 
-	def get_escape_moves(self):
-		return self.escape_moves
+    def get_escape_moves(self):
+        return self.escape_moves
